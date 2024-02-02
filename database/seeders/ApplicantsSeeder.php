c22583465517e5cbea10ab0f88e93148ee2f29b3
HolyD2ck <petranatolevic102@gmail.com>
2024-02-02 16:35:03 +0800

    factory

---
+++ database/seeders/ApplicantsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Applicants;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApplicantsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Applicants::factory()->count(100)->create(); 
+    }
+}
---
+++ database/seeders/EmployersSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Employers;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Employers::factory()->count(100)->create(); 
+    }
+}
